0f7054bc80c61bae9749bfdefb4768a9f79bcf8d
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-01-18 10:58:16 +0100

    Add project files.

---
+++ RestClient/Amount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClient
+{
+
+    public class Amount
+    {
+        public double value { get; set; }
+        public string currency { get; set; }
+    }
+
+}
---
+++ RestClient/Program.cs
@@ -0,0 +1,9 @@
+﻿using RestClient;
+using System.Text.Json;
+
+HttpClient client = new();
+
+var stream = client.GetStreamAsync("https://localhost:7270/api/UsdToDkk?value=5");
+var result = await JsonSerializer.DeserializeAsync<Amount>(await stream);
+
+Console.WriteLine($"{result.value} {result.currency}");
