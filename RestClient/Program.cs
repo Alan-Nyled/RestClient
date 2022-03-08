23597e66c1a311ea296ddded8eaff8de7fb2bd6b
alan-nyled <alannyled@gmail.com>
2022-03-08 15:38:40 +0100

    added user input

--- RestClient/Program.cs
+++ RestClient/Program.cs
@@ -7,7 +7,10 @@ async Task<Stream> GetData(string url)
     return await client.GetStreamAsync(url);
 }
 
-var people = GetData("https://swapi.dev/api/people/3");
+Console.WriteLine("Enter ID to search for, and hit Enter:");
+var id = Console.ReadLine();
+
+var people = GetData("https://swapi.dev/api/people/" + id);
 var person = await JsonSerializer.DeserializeAsync<People>(await people);
 var planet = GetData($"{person.homeworld}");
 var home = await JsonSerializer.DeserializeAsync<People>(await planet);
